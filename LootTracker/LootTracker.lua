db4942a6b5a4d0d441127e30e2e62d0188c65676
isitLoVe <isitl0v3@gmail.com>
2016-12-23 00:49:11 +0100

    0.2.1

--- LootTracker/LootTracker.lua
+++ LootTracker/LootTracker.lua
@@ -1,8 +1,28 @@
+local LootTrackerOptions_DefaultSettings = {
+	enabled = true,
+	uncommon = false,
+	common = false,
+	rare = true,
+	epic = true,
+	legendary = true
+}
+
+local function LootTracker_Initialize()
+	if not LootTrackerOptions  then
+		LootTrackerOptions = {};
+	end
+
+	for i in LootTrackerOptions_DefaultSettings do
+		if (not LootTrackerOptions[i]) then
+			LootTrackerOptions[i] = LootTrackerOptions_DefaultSettings[i];
+		end
+	end
+end
+	
 function LootTracker_OnLoad()
 
-	DEFAULT_CHAT_FRAME:AddMessage(string.format("LootTracker version %s by %s", GetAddOnMetadata("LootTracker", "Version"), GetAddOnMetadata("LootTracker", "Author")));
-    this:RegisterEvent("ADDON_LOADED");
-    this:RegisterEvent("CHAT_MSG_ADDON");   
+	DEFAULT_CHAT_FRAME:AddMessage(string.format("LootTracker v%s by %s", GetAddOnMetadata("LootTracker", "Version"), GetAddOnMetadata("LootTracker", "Author")));
+    this:RegisterEvent("VARIABLES_LOADED");
     this:RegisterEvent("CHAT_MSG_LOOT")
     
 	SlashCmdList["LootTracker"] = LootTracker_SlashCommand;
@@ -12,33 +32,31 @@ function LootTracker_OnLoad()
 	local MSG_PREFIX = "LootTracker"
 	
 	
-	lootpattern_looter = "^([^%s]+) receive loot" 
-	lootpattern_loot = "%[(.+)]"
+	LootTracker_pattern_looter = "^([^%s]+) receive" 
+	LootTracker_pattern_loot = "%[(.+)]"
+	LootTracker_pattern_rarity = "(.+)|c(.+)|H"
  	you = "You"
 	
-	LootTracker_color_common = "cffffffff"
-	LootTracker_color_uncommon = "cff1eff00"
-	LootTracker_color_rare = "cff0070dd"
-	LootTracker_color_epic = "cffa335ee"
-	LootTracker_color_legendary = "cffff8000"
+	LootTracker_color_common = "ffffffff"
+	LootTracker_color_uncommon = "ff1eff00"
+	LootTracker_color_rare = "ff0070dd"
+	LootTracker_color_epic = "ffa335ee"
+	LootTracker_color_legendary = "ffff8000"
 
-	LootTracker_enabled = true
 end
 
 function LootTracker_OnEvent()
-	if event == "CHAT_MSG_LOOT" and arg1 and LootTracker_enabled == true then
-		timestamp = date("%y-%m-%d %H:%M:%S")
+	if event == "VARIABLES_LOADED" then
+		this:UnregisterEvent("VARIABLES_LOADED");
+		LootTracker_Initialize();
+	elseif event == "CHAT_MSG_LOOT" and arg1 and LootTrackerOptions["enabled"] == true then
 		
 		if not LootTrackerDB then
 			LootTrackerDB = {}
 		end
 		
-		--check rarity
-		
-		
-
 		--extract the person who looted
-		local _, _, looter = string.find(arg1, lootpattern_looter)
+		local _, _, looter = string.find(arg1, LootTracker_pattern_looter)
 		if looter then
 			if looter == you then
 				looter = UnitName("player")
@@ -46,9 +64,31 @@ function LootTracker_OnEvent()
 		end
 		
 		--extract the loot
-		local _, _, loot = string.find(arg1, lootpattern_loot)
+		local _, _, loot = string.find(arg1, LootTracker_pattern_loot)
 		
-		--add loot to DB
+		--extract rarity
+		local _, _, _, rarity = string.find(arg1, LootTracker_pattern_rarity)
+
+		--check rarity
+		if rarity == LootTracker_color_common and LootTrackerOptions["common"] == true then
+			LootTracker_AddtoDB ( looter, loot )
+		elseif rarity == LootTracker_color_uncommon and LootTrackerOptions["uncommon"] == true then
+			LootTracker_AddtoDB ( looter, loot )
+		elseif rarity == LootTracker_color_rare and LootTrackerOptions["rare"] == true then
+			LootTracker_AddtoDB ( looter, loot )
+		elseif rarity == LootTracker_color_epic and LootTrackerOptions["epic"] == true then
+			LootTracker_AddtoDB ( looter, loot )
+		elseif rarity == LootTracker_color_legendary and LootTrackerOptions["legendary"] == true then
+			LootTracker_AddtoDB ( looter, loot )
+		end
+	end
+end
+
+function LootTracker_AddtoDB(looter, loot)
+
+	timestamp = date("%y-%m-%d %H:%M:%S")
+	
+	if looter and loot and timestamp and timestamp then
 		DEFAULT_CHAT_FRAME:AddMessage(timestamp .. " " .. looter .. " --> " .. loot)
 		if getn(LootTrackerDB) == 0 then
 			LootTrackerDB[1] = timestamp .. " " .. looter .. " --> " .. loot
@@ -58,34 +98,144 @@ function LootTracker_OnEvent()
 	end
 end
 
-function LootTracker_SlashCommand( msg )
+function LootTracker_Database()
+	DEFAULT_CHAT_FRAME:AddMessage("Dumping Database:")
+	for i in LootTrackerDB do 
+		DEFAULT_CHAT_FRAME:AddMessage(LootTrackerDB[i])
+	end
+end
+
+function LootTracker_SlashCommand(msg)
 
 	if msg == "help" then
-		DEFAULT_CHAT_FRAME:AddMessage("no help available yet")
+		DEFAULT_CHAT_FRAME:AddMessage("LootTracker DB is saved to: WTF\\Account\\ACCOUNTNAME\\SavedVariables\\LootTracker.lua")
+		DEFAULT_CHAT_FRAME:AddMessage("LootTracker usage:")
+		DEFAULT_CHAT_FRAME:AddMessage("/lt or /loottracker { help | enable | disable | toggle | show | database | reset | uncommon | common | rare | epic | legendary }")
+		DEFAULT_CHAT_FRAME:AddMessage(" - |cff9482c9help|r: prints out this help")
+		DEFAULT_CHAT_FRAME:AddMessage(" - |cff9482c9enable|r: enables loot tracking")
+		DEFAULT_CHAT_FRAME:AddMessage(" - |cff9482c9disable|r: disables loot tracking")
+		DEFAULT_CHAT_FRAME:AddMessage(" - |cff9482c9toggle|r: toggles loot tracking")
+		DEFAULT_CHAT_FRAME:AddMessage(" - |cff9482c9show|r: shows the current configuration")
+		DEFAULT_CHAT_FRAME:AddMessage(" - |cff9482c9sdatabase|r: shows the loot database")
+		DEFAULT_CHAT_FRAME:AddMessage(" - |cff9482c9reset|r: resets the loot database")
+		DEFAULT_CHAT_FRAME:AddMessage(" - |cff9482c9uncommon|r: toggles tracking uncommon loot")
+		DEFAULT_CHAT_FRAME:AddMessage(" - |cff9482c9common|r: toggles tracking common loot")
+		DEFAULT_CHAT_FRAME:AddMessage(" - |cff9482c9rare|r: toggles tracking rare loot")
+		DEFAULT_CHAT_FRAME:AddMessage(" - |cff9482c9epic|r: toggles tracking epic loot")
+		DEFAULT_CHAT_FRAME:AddMessage(" - |cff9482c9legendary|r: toggles tracking legendary loot")
 	elseif msg == "toggle" then
-		if LootTracker_enabled == true then
-			LootTracker_enabled = false
-			DEFAULT_CHAT_FRAME:AddMessage("LootTracker disabled")
-		elseif LootTracker_enabled == false then
-			LootTracker_enabled = true
-			DEFAULT_CHAT_FRAME:AddMessage("LootTracker enabled")
+		if LootTrackerOptions["enabled"] == true then
+			LootTrackerOptions["enabled"] = false
+			DEFAULT_CHAT_FRAME:AddMessage("LootTracker state: |cffff0000disabled|r")
+		elseif LootTrackerOptions["enabled"] == false then
+			LootTrackerOptions["enabled"] = true
+			DEFAULT_CHAT_FRAME:AddMessage("LootTracker state: |cff00ff00enabled|r")
 		end
 	elseif msg == "enable" then
-		LootTracker_enabled = true
-		DEFAULT_CHAT_FRAME:AddMessage("LootTracker enabled")
+		LootTrackerOptions["enabled"] = true
+			DEFAULT_CHAT_FRAME:AddMessage("LootTracker state: |cff00ff00enabled|r")
 	elseif msg == "disable" then
-		LootTracker_enabled = false
-		DEFAULT_CHAT_FRAME:AddMessage("LootTracker disabled")
+		LootTrackerOptions["enabled"] = false
+			DEFAULT_CHAT_FRAME:AddMessage("LootTracker state: |cffff0000disabled|r")
+	elseif msg == "show" then
+	
+		if LootTrackerOptions["enabled"] == true then
+			DEFAULT_CHAT_FRAME:AddMessage("LootTracker state: |cff00ff00enabled|r")
+		elseif LootTrackerOptions["enabled"] == false then
+			DEFAULT_CHAT_FRAME:AddMessage("LootTracker state: |cffff0000disabled|r")
+		end
+	
+		if LootTrackerOptions["common"] == true then
+			DEFAULT_CHAT_FRAME:AddMessage("Tracking |cffffffffcommon|r loot: |cff00ff00enabled|r")
+		elseif LootTrackerOptions["common"] == false then
+			DEFAULT_CHAT_FRAME:AddMessage("Tracking |cffffffffcommon|r loot: |cffff0000disabled|r")
+		end
+
+		if LootTrackerOptions["uncommon"] == true then
+			DEFAULT_CHAT_FRAME:AddMessage("Tracking |cff1eff00uncommon|r loot: |cff00ff00enabled|r")
+		elseif LootTrackerOptions["uncommon"] == false then
+			DEFAULT_CHAT_FRAME:AddMessage("Tracking |cff1eff00uncommon|r loot: |cffff0000disabled|r")
+		end
+
+		if LootTrackerOptions["rare"] == true then
+			DEFAULT_CHAT_FRAME:AddMessage("Tracking |cff0070ddrare|r loot: |cff00ff00enabled|r")
+		elseif LootTrackerOptions["rare"] == false then
+			DEFAULT_CHAT_FRAME:AddMessage("Tracking |cff0070ddrare|r loot: |cffff0000disabled|r")
+		end
+
+		if LootTrackerOptions["epic"] == true then
+			DEFAULT_CHAT_FRAME:AddMessage("Tracking |cffa335eeepic|r loot: |cff00ff00enabled|r")
+		elseif LootTrackerOptions["epic"] == false then
+			DEFAULT_CHAT_FRAME:AddMessage("Tracking |cffa335eeepic|r loot: |cffff0000disabled|r")
+		end
+
+		if LootTrackerOptions["legendary"] == true then
+			DEFAULT_CHAT_FRAME:AddMessage("Tracking |cffff8000legendary|r loot: |cff00ff00enabled|r")
+		elseif LootTrackerOptions["legendary"] == false then
+			DEFAULT_CHAT_FRAME:AddMessage("Tracking |cffff8000legendary|r loot: |cffff0000disabled|r")
+		end
+
+	elseif msg == "database" then
+		LootTracker_Database();
+	elseif msg == "reset" then
+		LootTrackerDB = {}
+		DEFAULT_CHAT_FRAME:AddMessage("Loot Database has been reset")
+	elseif msg == "common" then		
+		if LootTrackerOptions["common"] == true then
+			LootTrackerOptions["common"] = false
+			DEFAULT_CHAT_FRAME:AddMessage("Tracking common loot: |cffff0000disabled|r")
+		elseif LootTrackerOptions["common"] == false then
+			LootTrackerOptions["common"] = true
+			DEFAULT_CHAT_FRAME:AddMessage("Tracking common loot: |cff00ff00enabled|r")
+		end
+	elseif msg == "uncommon" then		
+		if LootTrackerOptions["uncommon"] == true then
+			LootTrackerOptions["uncommon"] = false
+			DEFAULT_CHAT_FRAME:AddMessage("Tracking uncommon loot: |cffff0000disabled|r")
+		elseif LootTrackerOptions["uncommon"] == false then
+			LootTrackerOptions["uncommon"] = true
+			DEFAULT_CHAT_FRAME:AddMessage("Tracking uncommon loot: |cff00ff00enabled|r")
+		end
+	elseif msg == "rare" then		
+		if LootTrackerOptions["rare"] == true then
+			LootTrackerOptions["rare"] = false
+			DEFAULT_CHAT_FRAME:AddMessage("Tracking rare loot: |cffff0000disabled|r")
+		elseif LootTrackerOptions["rare"] == false then
+			LootTrackerOptions["rare"] = true
+			DEFAULT_CHAT_FRAME:AddMessage("Tracking rare loot: |cff00ff00enabled|r")
+		end
 	elseif msg == "epic" then		
-		if LootTracker_epic == true then
-			LootTracker_epic = false
-			DEFAULT_CHAT_FRAME:AddMessage("LootTracker will NOT track Epic items")
-		elseif LootTracker_enabled == false then
-			LootTracker_epic = true
-			DEFAULT_CHAT_FRAME:AddMessage("LootTracker is now tracking Epic items")
+		if LootTrackerOptions["epic"] == true then
+			LootTrackerOptions["epic"] = false
+			DEFAULT_CHAT_FRAME:AddMessage("Tracking epic loot: |cffff0000disabled|r")
+		elseif LootTrackerOptions["epic"] == false then
+			LootTrackerOptions["epic"] = true
+			DEFAULT_CHAT_FRAME:AddMessage("Tracking epic loot: |cff00ff00enabled|r")
+		end
+	elseif msg == "legendary" then		
+		if LootTrackerOptions["legendary"] == true then
+			LootTrackerOptions["legendary"] = false
+			DEFAULT_CHAT_FRAME:AddMessage("Tracking legendary loot: |cffff0000disabled|r")
+		elseif LootTrackerOptions["legendary"] == false then
+			LootTrackerOptions["legendary"] = true
+			DEFAULT_CHAT_FRAME:AddMessage("Tracking legendary loot: |cff00ff00enabled|r")
 		end
 	else
-		DEFAULT_CHAT_FRAME:AddMessage("no help available yet")
+		DEFAULT_CHAT_FRAME:AddMessage("LootTracker DB is saved to: WTF\Account\ACCOUNTNAME\SavedVariables\LootTracker.lua")
+		DEFAULT_CHAT_FRAME:AddMessage("LootTracker usage:")
+		DEFAULT_CHAT_FRAME:AddMessage("/lt or /loottracker { help | enable | disable | toggle | show | database | reset | uncommon | common | rare | epic | legendary }")
+		DEFAULT_CHAT_FRAME:AddMessage(" - |cff9482c9help|r: prints out this help")
+		DEFAULT_CHAT_FRAME:AddMessage(" - |cff9482c9enable|r: enables loot tracking")
+		DEFAULT_CHAT_FRAME:AddMessage(" - |cff9482c9disable|r: disables loot tracking")
+		DEFAULT_CHAT_FRAME:AddMessage(" - |cff9482c9toggle|r: toggles loot tracking")
+		DEFAULT_CHAT_FRAME:AddMessage(" - |cff9482c9show|r: shows the current configuration")
+		DEFAULT_CHAT_FRAME:AddMessage(" - |cff9482c9sdatabase|r: shows the loot database")
+		DEFAULT_CHAT_FRAME:AddMessage(" - |cff9482c9reset|r: resets the loot database")
+		DEFAULT_CHAT_FRAME:AddMessage(" - |cff9482c9uncommon|r: toggles tracking uncommon loot")
+		DEFAULT_CHAT_FRAME:AddMessage(" - |cff9482c9common|r: toggles tracking common loot")
+		DEFAULT_CHAT_FRAME:AddMessage(" - |cff9482c9rare|r: toggles tracking rare loot")
+		DEFAULT_CHAT_FRAME:AddMessage(" - |cff9482c9epic|r: toggles tracking epic loot")
+		DEFAULT_CHAT_FRAME:AddMessage(" - |cff9482c9legendary|r: toggles tracking legendary loot")
 	end
 
 end
